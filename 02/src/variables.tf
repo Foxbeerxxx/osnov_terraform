a4e6ddc4cd100eb49bbb76d9339c2f05b3f79796
Alexey Tatarintsev <tatarintsev0810@gmail.com>
2025-06-03 23:03:50 +0300

    2

--- 02/src/variables.tf
+++ 02/src/variables.tf
@@ -1,25 +1,34 @@
 ###cloud vars
 
+variable "token" {
+  description = "IAM token for Yandex Cloud"
+  type        = string
+  default     = "y0__xCT0ufIBxjB3RMggtW4jxOA51ZsI3f2sKmFlatvU579i7Vgfw"
+}
+
 
 variable "cloud_id" {
   type        = string
-  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+  default     = "b1gvjpk4qbrvling8qq1"
+  description = "Cloud ID in Yandex.Cloud"
 }
 
 variable "folder_id" {
   type        = string
-  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+  default     = "b1gse67sen06i8u6ri78"
+  description = "Folder ID in Yandex.Cloud"
 }
 
 variable "default_zone" {
   type        = string
   default     = "ru-central1-a"
-  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+  description = "Availability zone"
 }
+
 variable "default_cidr" {
   type        = list(string)
   default     = ["10.0.1.0/24"]
-  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+  description = "Subnet CIDR block"
 }
 
 variable "vpc_name" {
@@ -33,6 +42,6 @@ variable "vpc_name" {
 
 variable "vms_ssh_root_key" {
   type        = string
-  default     = "<your_ssh_ed25519_key>"
+  default     = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM5m9AhMRWiO+yybLYEHaJhFaODFZDTbOiYqitAxzWgs alexey@dell"
   description = "ssh-keygen -t ed25519"
 }
